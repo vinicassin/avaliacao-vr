059396e15787a398e502242f2a0e13aefef229ad
vinicius_cassin <vinicius.cassin@hotmail.com>
2023-07-22 17:38:38 -0300

    Fix some problems

--- teste_ui/cypress.config.js
+++ teste_ui/cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight:1080,
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/report/mochawesome-report',
@@ -10,6 +12,7 @@ module.exports = defineConfig({
     timestamp: 'mmddyyyy_HHMMss'
   },
   e2e: {
+    baseUrl: "https://vr.com.br/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- teste_ui/cypress/e2e/spec_menus.cy.js
+++ teste_ui/cypress/e2e/spec_menus.cy.js
@@ -1,23 +1,16 @@
 describe('Validar menus do site VR', () => {
 
   beforeEach(() => {
-    cy.viewport(1920, 1080);
-    cy.visit('https://vr.com.br');
+    cy.visit('/');
   })
 
   it('Validar mapa de estabelecimentos para usar cartão VR', () => {
-    // cy.visit('https://vr.com.br/onde-aceita.htm');
-
     cy.contains('a', 'Pra Você').click();
     cy.contains('a', 'Onde usar o meu cartão').click();
+    cy.intercept('GET', '/maps/api/mapsjs/gen_204?csp_test=true').as('getMaps')
     cy.get('#map').should('be.visible');
 
-    cy.request({
-      method: 'GET',
-      url: 'https://maps.googleapis.com/maps/api/mapsjs/gen_204?csp_test=true'
-    }).then((res) => {
-      expect(res.status).to.be.equal(200)
-    })
+    cy.wait('@getMaps').its('response.statusCode').should('eq', 200)
   })
 
 })
